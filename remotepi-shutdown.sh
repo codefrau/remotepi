4127be3a462ddc3dc602cb6341f683b9ff091ccd
Bert Freudenberg <bert@freudenbergs.de>
2016-08-30 23:08:58 +0200

    Properly trigger power button to initiate shutdown

--- remotepi-shutdown.sh
+++ remotepi-shutdown.sh
@@ -1,26 +1,30 @@
 #!/bin/bash
 
-# this is the GPIO pin receiving the shut-down signal
-GPIOpin1=14
+# this is the GPIO pin triggering the power button
+GPIO_TRIGGER=15
 
-cut_the_power () {
-    echo "$GPIOpin1" > /sys/class/gpio/export 2>/dev/null
-    echo "out" > /sys/class/gpio/gpio$GPIOpin1/direction
-    echo "1" > /sys/class/gpio/gpio$GPIOpin1/value
+# this is the GPIO pin signaling that shutdown is done
+GPIO_DONE=14
+
+trigger_power_button () {
+  echo "$GPIO_TRIGGER" > /sys/class/gpio/export
+  echo "out" > /sys/class/gpio/gpio$GPIO_TRIGGER/direction
+  echo "1" > /sys/class/gpio/gpio$GPIO_TRIGGER/value
+  usleep 125000
+  echo "0" > /sys/class/gpio/gpio$GPIO_TRIGGER/value
+  usleep 200000
+  echo "1" > /sys/class/gpio/gpio$GPIO_TRIGGER/value
+  usleep 400000
+  echo "0" > /sys/class/gpio/gpio$GPIO_TRIGGER/value
+
+  echo "$GPIO_DONE" > /sys/class/gpio/export
+  echo "out" > /sys/class/gpio/gpio$GPIO_DONE/direction
+  echo "1" > /sys/class/gpio/gpio$GPIO_DONE/value
+
+  usleep 200000
 }
 
-case "$1" in
-  halt)
-    # ignore
-    ;;
-  poweroff)
-    # turn of power via RemotePi
-    cut_the_power
-    ;;
-  reboot)
-    # ignore
-    ;;
-  *)
-    # ignore
-    ;;
-esac
+
+if [ "$1" != "reboot" ]; then
+    trigger_power_button
+fi
